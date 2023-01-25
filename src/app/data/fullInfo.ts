12145112d1c3cc6a6b1e8aeff80a00ad373f2bf6
Roman Tkachuk <tkachukrr@gmail.com>
2023-01-25 12:58:22 +0100

    added data.ts

---
+++ src/app/data/fullInfo.ts
@@ -0,0 +1,29 @@
+const fullInfo = Object.freeze({
+  firstName: {
+    en: 'Roman',
+    pl: 'Roman',
+    ua: 'Роман',
+  },
+
+  lastName: {
+    en: 'Tkachuk',
+    pl: 'Tkaczuk',
+    ua: 'Ткачук',
+  },
+
+  dateOfBirh: '1987-08-27T00:00:00',
+
+  location: {
+    en: 'Warsaw, before the war - Kharkiv',
+    pl: 'Warszawa, przed wojną – Charków',
+    ua: 'Варшава, до війни - Харків',
+  },
+
+  intro: {
+    en: 'Looking for Junior Frontend developer position. I am highly motivated to get new knowledge and put it to use in real projects. I am good team player and prefer to build strong communication via members of a group. Also, I am well-organized and responsible. I am constantly engaged in self-development in programming and improvement of English language skills.',
+    pl: 'Szukam pracy na stanowisku Junior Frontend Developer. Jestem bardzo zmotywowany do zdobywania nowych umiejętności i wykorzystywania ich przy pracy nad projektami. Dobrze odnajduję się w pracy zespołowej i lubię budować silne relacje w grupie współpracowników. Jestem osobą dobrze zorganizowaną i odpowiedzialną. Stale zajmuję się samorozwojem w zakresie programowania i doskonaleniem znajomości języka angielskiego.',
+    ua: 'Шукаю посаду Junior Frontend розробника. Маю велику мотивацію отримувати нові знання та застосовувати їх на реальних проектах. Я хороший командний гравець та віддаю перевагу налагодженню комунікацій всередині команди. Крім того, маю організаційні навички та відповідально ставлюся до виконання задач. Постійно займаюся саморозвитком у програмуванні та вдосконаленням знань англійської мови.',
+  },
+
+  subinfo: 'I am considering work in all cities and am ready to move.',
+});
