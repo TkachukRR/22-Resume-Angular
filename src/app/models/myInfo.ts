5f2f9b27af9609cb08932370c724bd20becf04cb
Roman Tkachuk <tkachukrr@gmail.com>
2023-01-25 13:04:55 +0100

    added data interface models/myInfo.ts

---
+++ src/app/models/myInfo.ts
@@ -0,0 +1,29 @@
+export interface MyInfo {
+  firstName: {
+    en: string;
+    pl: string;
+    ua: string;
+  };
+
+  lastName: {
+    en: string;
+    pl: string;
+    ua: string;
+  };
+
+  dateOfBirh: string;
+
+  location: {
+    en: string;
+    pl: string;
+    ua: string;
+  };
+
+  intro: {
+    en: string;
+    pl: string;
+    ua: string;
+  };
+
+  subinfo: string;
+}
