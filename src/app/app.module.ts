36e771dceb37b19a0c18c0afed691120c6006df6
Roman Tkachuk <tkachukrr@gmail.com>
2023-01-25 12:21:23 +0100

    Added introduce component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndroduceComponent } from './components/introduce/introduce.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IndroduceComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
